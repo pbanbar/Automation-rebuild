823b809d2bc2a60ebd361a19811b443ac5979383
Pravin Bankar <pravinbankar143@gmail.com>
2020-11-03 09:35:22 +0530

    adding row three

--- hello world.py
+++ hello world.py
@@ -1,6 +1,6 @@
 Object1="this is the first programm i am printing on console"
 print(Object1)
-lets start with new learing
+# lets start with new learing
 
 for i in range(1,101):
     if i%2==0:
@@ -11,6 +11,7 @@ for i in range(1,11):
     print(i*2)
 for i in range(1,11):
     print(i*3)
+    print(i*4)
 #adding print statement
 
 # select="prav1345inc2h3a4n5dra5she7kh8a9rbankar"
