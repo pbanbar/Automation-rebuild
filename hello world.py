5442802ac84c4fae27320fcdb02d177567c89e66
Pravin Bankar <pravinbankar143@gmail.com>
2020-11-03 12:36:00 +0530

    removing the table

--- hello world.py
+++ hello world.py
@@ -11,11 +11,11 @@ for i in range(1,11):
     print(i*2)
 for i in range(1,11):
     print(i*3)
-    print(i*4)
-    print(i*5)
-    print(i*6)
-    print(i*7)
-    print(i*8)
+    # print(i*4)
+    # print(i*5)
+    # print(i*6)
+    # print(i*7)
+    # print(i*8)
 #adding print statement
 
 # select="prav1345inc2h3a4n5dra5she7kh8a9rbankar"
